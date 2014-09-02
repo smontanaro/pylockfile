4eb27075e0523b6ed2027444fd42d83a6e9b14c5
Skip Montanaro <skipm@trdlnk.com>
2014-09-02 07:58:33 -0500

    date

--- doc/conf.py
+++ doc/conf.py
@@ -36,7 +36,7 @@ master_doc = 'lockfile'
 
 # General substitutions.
 project = 'lockfile'
-copyright = '2008, Skip Montanaro'
+copyright = '2014, Skip Montanaro'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
